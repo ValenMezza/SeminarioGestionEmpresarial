4140a0151e4033a8f633cbce479d359f2f0289ed
Valentino Mezzavilla <mezza12354@gmail.com>
2026-03-03 00:48:20 -0300

    Estilos de venta ok

--- store/dbStock.js
+++ store/dbStock.js
@@ -1,10 +1,17 @@
 const dbStock = {
   productos: [
-    { id: 1, producto: "Tierra", precio: 19500, stock:10 },
-    { id: 2, producto: "Arena", precio: 10000,stock:10 },
-    { id: 3, producto: "Piedra", precio: 15000, stock:10 },
-    { id: 4, producto: "Arenza zarandeada", precio: 12000, stock:0 },
-    { id: 5, producto: "Material Relleno", precio: 20000 , stock: 1}
+    { id: 1, producto: "Tierra", precio: 19500, stock: 10 },
+    { id: 2, producto: "Arena", precio: 10000, stock: 10 },
+    { id: 3, producto: "Piedra", precio: 15000, stock: 10 },
+    { id: 4, producto: "Arena Zarandeada", precio: 12000, stock: 0 },
+    { id: 5, producto: "Ripio", precio: 14000, stock: 6 },
+    { id: 6, producto: "Piedra 6/20", precio: 16000, stock: 8 },
+    { id: 7, producto: "Piedra 10/30", precio: 16500, stock: 5 },
+    { id: 8, producto: "Material de Relleno", precio: 20000, stock: 1 },
+    { id: 9, producto: "Tosca", precio: 18000, stock: 0 },
+    { id: 10, producto: "Granito Triturado", precio: 22000, stock: 3 },
+    { id: 11, producto: "Polvo de Piedra", precio: 13000, stock: 9 },
+    { id: 12, producto: "Canto Rodado", precio: 21000, stock: 2 }
   ],
 };
 
@@ -28,4 +35,4 @@ async function prodsById(id) {
 
 
 
-module.exports = { dbStock, listProds, prodsById};
+module.exports = { dbStock, listProds, prodsById };
