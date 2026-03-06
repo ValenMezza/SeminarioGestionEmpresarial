4aee8421f8e8ea09b874cd1868a8622354a78df5
Valentino Mezzavilla <mezza12354@gmail.com>
2026-03-06 00:30:36 -0300

    contenedores

--- controllers/alquiler.controller.js
+++ controllers/alquiler.controller.js
@@ -1,15 +1,19 @@
-const  alquilerController ={
-    index: (req, res) =>{
-        res.render('alquileres/index')
+const { listContenedores } = require("../store/dbContenedor")
+
+const alquilerController = {
+    index: async (req, res) => {
+        const contenedores = await listContenedores();
+
+        res.render('alquileres/index', { contenedores })
     },
-    detalle :(req, res) =>{
+    detalle: (req, res) => {
         res.render('alquileres/detalle')
     },
-    nuevoAlquiler: (req,res)=>{
-        res.render ('alquileres/nuevo_alquiler')
+    nuevoAlquiler: (req, res) => {
+        res.render('alquileres/nuevo_alquiler')
     },
-    edicionAlquiler:(req,res) =>{
-        res.render ('alquileres/edicion_alquiler')
+    edicionAlquiler: (req, res) => {
+        res.render('alquileres/edicion_alquiler')
     }
 }
 
--- store/dbContenedor.js
+++ store/dbContenedor.js
@@ -2,9 +2,9 @@
 const dbContenedores = {
     listaContenedores: [
         { id: 1, estado: "Disponible", precioAlquiler: 15000, precioDia: 5000, cliente: null, inicioAlquiler: null, finAlquiler: null, direccionAlquiler: null },
-        { id: 2, estado: "Alquilado", precioAlquiler: 15000, precioDia: 5000, cliente: "Juan Perez", inicioAlquiler: "2024-06-01", finAlquiler: "2024-06-10", direccionAlquiler: "Calle Falsa 123" },
+        { id: 2, estado: "Disponible", precioAlquiler: 15000, precioDia: 5000, cliente: "Juan Perez", inicioAlquiler: "2024-06-01", finAlquiler: "2024-06-10", direccionAlquiler: "Calle Falsa 123" },
         { id: 3, estado: "Disponible", precioAlquiler: 15000, precioDia: 5000, cliente: null, inicioAlquiler: null, finAlquiler: null, direccionAlquiler: null },
-        { id: 4, estado: "Alquilado", precioAlquiler: 15000, precioDia: 5000, cliente: "Maria Lopez", inicioAlquiler: "2024-06-05", finAlquiler: "2024-06-15", direccionAlquiler: "Avenida Siempre Viva 456" },
+        { id: 4, estado: "Disponible", precioAlquiler: 15000, precioDia: 5000, cliente: "Maria Lopez", inicioAlquiler: "2024-06-05", finAlquiler: "2024-06-15", direccionAlquiler: "Avenida Siempre Viva 456" },
         { id: 5, estado: "Disponible", precioAlquiler: 15000, precioDia: 5000, cliente: null, inicioAlquiler: null, finAlquiler: null, direccionAlquiler: null }
     ],
 };
